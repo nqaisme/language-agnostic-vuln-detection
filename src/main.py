721c3efccf8270abaa91ec794a158682239a1f58
steevewu <nqaisme@icloud.com>
2026-08-05 09:01:37 +0000

    shang

--- src/main.py
+++ src/main.py
@@ -5,33 +5,24 @@ from tmp import tmp_dataset
 from datasets import load_from_disk
 
 def save_tensor(tensor: dict, split: str, type: str, args):
-    if not args.output_dir:
-        output_dir = os.path.join(os.getcwd(), 'tensors')
+    if not args.result_dir:
+        result_dir = os.path.join(os.getcwd(), 'tensors')
     else:
-        output_dir = os.path.join(args.output_dir, 'tensors')
+        result_dir = os.path.join(args.output_dir, 'tensors')
 
-    os.makedirs(output_dir, exist_ok=True)
-        
-    
-    split_dir = os.path.join(output_dir, split)
+    os.makedirs(result_dir, exist_ok=True)
+    split_dir = os.path.join(result_dir, split)
     os.makedirs(split_dir, exist_ok=True)
-    torch.save(tensor, os.path.join(split_dir, f'{type}.pt'))
-            
+    torch.save(tensor, os.path.join(split_dir, f'{type}.pt'))            
+    print(f'embedding tensor saved successfully to {os.path.join(split_dir, f'{type}.pt')}!\n')
 
-    print(f'Embedding tensor saved successfully to {os.path.join(split_dir, f'{type}.pt')}!\n')
-    
-        
 
 def build_features(dataset, args):
     cx = extractor(VARS.CB, batch_size=args.batch_size, max_length=args.max_length)
     gcx = extractor(VARS.GCB, batch_size=args.batch_size, max_length=args.max_length)
     builder = neutral_feature_builder()
     
-    
-    
     result = {}
-    
-    
     for split in ['train', 'test']:
         result[split] = {}
         for type in ['ec', 'eg', 'nf']:
@@ -42,70 +33,34 @@ def build_features(dataset, args):
                     ebd = gcx(list(dataset[split]['function']))
                 case 'nf':
                     ebd = builder.build(result[split]['ec'].X, result[split]['eg'].X)
-                
-
+            
             result[split][type] = ds_sample(
                 ebd,
                 np.array(dataset[split]['label']),
                 type
             )
-            
             save_tensor(ebd, split, type, args)
-            
     return result
 
 def parse_args():
     parser = argparse.ArgumentParser()
     
-    
-    parser.add_argument(
-        '--dataset-path',
-        default=None,
-        type=str,
-        help='path to the dataset directory, consists of train & test set',
-        required=True
-    )
-    
-    parser.add_argument(
-        '--weights-path',
-        default=None,
-        type=str,
-        help='path to model\' weights directory',
-        required=True
-    )
-    
-    parser.add_argument(
-        '--features-path',
-        default=None,
-        type=str,
-        help='path to embedding tensors directory',
-        required=True
-    )
-    
-    
     parser.add_argument(
         '--source-col',
         type=str,
-        default='func',
+        default=None,
         help='name of column which contains source code',
         required=True
     )
-
+    
     parser.add_argument(
         '--label-col',
         type=str,
-        default='target',
+        default=None,
         help='name of column which contains label of source code',
         required=True
     )
-    
-    parser.add_argument(
-        '--output-dir',
-        default=None,
-        type=str,
-        required=True
-    )
-    
+        
     parser.add_argument(
         '--max-iter',
         type=int,
@@ -166,6 +121,33 @@ def parse_args():
         action='store_true'
     )
     
+    parser.add_argument(
+        '--dataset-dir',
+        type=str,
+        default=None,
+        required=True,
+    )
+    
+    parser.add_argument(
+        '--result-dir',
+        type=str,
+        default=None,
+        required=True
+    )
+    
+    parser.add_argument(
+        '--result-subdir',
+        type=str,
+        default=None,
+        required=True
+    )
+    
+    parser.add_argument(
+        '--tensor-dir',
+        type=str,
+        defaut='tensors',
+    )
+    
     
     
     parser.add_argument()
@@ -180,7 +162,7 @@ def main(args):
     #         {args.source_col: 'function', args.label_col: 'label'}
     #         )
     
-    dataset = load_from_disk(args.dataset_path).rename_columns(
+    dataset = load_from_disk(args.dataset_dir).rename_columns(
         {
             args.source_col: 'function',
             args.label_col: 'label'
@@ -188,8 +170,8 @@ def main(args):
     )
     
     
-    dir_name = datetime.datetime.now().strftime('%Y%m%d_%H%M')
-    args.output_dir = os.path.join(args.output_dir, dir_name)
+    subdir_name = datetime.datetime.now().strftime('%Y%m%d_%H%M')
+    args.result_dir = os.path.join(args.result_dir, subdir_name)
 
     
     if args.do_train:
@@ -198,34 +180,31 @@ def main(args):
         results = {}
         
         for type in ['ec', 'eg', 'nf']:
-            results[type] = classifier.train(features['train'][type], **{"output_dir": args.output_dir, 'dataset': args.dataset})
+            results[type] = classifier.train(features['train'][type], **{"output_dir": args.result_dir, 'dataset': os.path.basename(args.dataset_dir)})
     
     if args.do_test:
         weight_paths = {
-            'ec': os.path.join(args.weights_path, 'ec_weights.pkl'),
-            'eg': os.path.join(args.weights_path, 'eg_weights.pkl'),
-            'nf': os.path.join(args.weights_path, 'nf_weights.pkl')
+            'ec': os.path.join(args.result_dir, args.result_subdir, 'ec_weights.pkl'),
+            'eg': os.path.join(args.result_dir, args.result_subdir, 'eg_weights.pkl'),
+            'nf': os.path.join(args.result_dir, args.result_subdir, 'nf_weights.pkl'),            
         }
-        features = {
-            'ec': torch.load(os.path.join(args.features_path, 'test/ec.pt')),
-            'eg': torch.load(os.path.join(args.features_path, 'test/eg.pt')),
-            'nf': torch.load(os.path.join(args.features_path, 'test/nf.pt'))
+        tensor_paths = {
+            'ec': os.path.join(args.result_dir, args.result_subdir, args.tensor_dir, '/test/ec.pt'),
+            'eg': os.path.join(args.result_dir, args.result_subdir, args.tensor_dir, '/test/eg.pt'),
+            'nf': os.path.join(args.result_dir, args.result_subdir, args.tensor_dir, '/test/nf.pt'),
         }
         
-        y_test = np.array(dataset['test']['label'])
+        y_true = np.array(dataset['test']['label'])
         
-        for type, weight in weight_paths.items():
-            print(simple_classifier.evaluate(
-                weight,
-                ds_sample(features[type], y_test, type)
-            ))
-            
-        
-        
-        
-        
-        
-    
+        for type, weight_file in weight_paths.items():
+            sample = ds_sample(
+                torch.load(tensor_paths[type]),
+                y_true,
+                type
+            )
+            print(f"{type} embedding result:\t{simple_classifier.evaluate(weight_file, sample)}")
+
+
 if __name__ == '__main__':
     args = parse_args()
     main(args)
